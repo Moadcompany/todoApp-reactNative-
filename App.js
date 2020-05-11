12ae008d4c4ea2de1b90af54aaacc6beb7b30d64
mouad.zizi <mouadzizi@users.noreply.github.com>
2020-05-11 20:09:16 +0000

    MedDoc application

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+
+export default function App() {
+const [people, setPeople] = useState([
+
+  {name: 'moad', id:'1'},
+  {name: 'moad2', id:'2'},
+  {name: 'moad3', id:'3'},
+  {name: 'moad4', id:'4'},
+  {name: 'moad5', id:'5'},
+  {name: 'moad6', id:'6'},
+
+]);
+
+  return (
+    <View style={styles.container}>
+
+    {people.map((v) => {
+      return (
+        <View> 
+        <Text> {v.name} </Text>
+        </View>
+      )
+    } )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
+
+
